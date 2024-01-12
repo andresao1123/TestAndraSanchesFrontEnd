5d364942a779735b31a6291ffbab4f82a9f3344c
andres sanchez <andres.sanchez.perez@udla.edu.ec>
2024-01-11 19:56:54 -0500

    Update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    apiUrl: '64.23.134.60:3000',
+    apiUrl: 'http://64.23.134.60:3000',
     secretKey:'d7e90f301add8327b67be184236df280b7f023a9dfc792aff1e1d9776644ee8f'
     // Other configuration options...
   };
